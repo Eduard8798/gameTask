131e601e89308d204066c30b31a3b8824f5fb1e7
Eduard8798 <eduard9999997@gmail.com>
2025-08-24 11:25:28 +0300

    deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import './main.css'
 import HeroList from "./page/heroList/HeroList.jsx";
 import HeroInfo from "./page/heroInfo/HeroInfo.jsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import HeroEdit from "./page/heroEdit/HeroEdit.jsx";
 import HeroCreate from "./page/createHero/HeroCreate.jsx";
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
-                    <Route path="/" element={<HeroList/>}/>
-                    <Route path="/HeroInfo/:id" element={<HeroInfo/>}/>
-                    <Route path="/HeroEdit/:id" element={<HeroEdit/>}/>
-                    <Route path="/HeroCreate" element={<HeroCreate/>}/>
+                <Route path="/" element={<HeroList />} />
+                <Route path="/HeroInfo/:id" element={<HeroInfo />} />
+                <Route path="/HeroEdit/:id" element={<HeroEdit />} />
+                <Route path="/HeroCreate" element={<HeroCreate />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
